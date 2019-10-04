c2801a7897357816e42288e5747037fdadd4a7e7
sephoro <eliassepuru@gmail.com>
2019-10-04 15:12:23 +0200

    Message ready for modulation

--- Code/main.m
+++ Code/main.m
@@ -4,8 +4,8 @@ clear
 % Foward Error Correction
 % BCH Encoder and Decoder
 
-M = 3;
-N = 2^M-1;                % Codeword length
+m = 3;
+N = 2^m-1;                % Codeword length
 K = 4;                    % Message length
 codeRate = K/N;           % Code rate of the FEC code
 t = bchnumerr(N,K);       % Get the error correcting capability
@@ -14,25 +14,33 @@ G = bchgenpoly(N,K);      % Get the generator Polynomial
 encoder = comm.BCHEncoder(N,K,G);	% BCH encoder
 decoder = comm.BCHDecoder(N,K,G);   % BCH decoder
 
+% Modulation
+% 16 QAM Modulator
+
+M = 16;                     % Size of signal constellation
+b = log2(M);                % Number of bits per symbol
+
 % Lets create the message
 
-nwords = 1;
-msgTx = [1;0;0;0];
-
+msgTx = randi([0 1],K*log2(M),1); % Make the message a factor the FEC and Modulation order
+disp(msgTx')  
 
 % Lets Encode
 
-encTx = encoder(msgTx)';
-disp(encTx)
+encTx = encoder(msgTx);
+disp(encTx')
 disp(newline)
 
+%{ 
 % Introduce noise
 
-noisycode = mod(encTx + randerr(nwords,N,1:t),2)';
-disp(noisycode')
-disp(newline)
+ noisycode = mod(encTx + randerr(1,N*4,1:4)',2);
+ disp(noisycode')
+ disp(newline)
+ disp(isequal(noisycode,encTx));
 
-
+%}
+ 
 % Decode
 
 msgRx = decoder(noisycode);
