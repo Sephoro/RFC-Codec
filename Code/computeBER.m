26066b0c89e871242e09db17b77425a027f84ee0
sephoro <eliassepuru@gmail.com>
2019-10-06 01:47:03 +0200

    Refactored the codec functionality

---
+++ Code/computeBER.m
@@ -0,0 +1,109 @@
+function [BER,S,NS] = computeBER(m,MessageLength,ModulationOrder,EbNo,NumBits)
+
+     %This function computes the BER of any combination of BCH FEC and QAM
+     ...modulation configuration given EbNo. The functions returns the BER.
+     
+ 
+    % Get the variables in a more farmiliar format
+    
+        k = MessageLength;
+        M = ModulationOrder;
+        BER = zeros(1, length(EbNo));
+
+    % Get the BCH encoder and decoder
+
+        [encoder,decoder,codeRate] = bchFEC(m,k);
+
+    % Error Collecor
+
+        errorRate = comm.ErrorRate;
+        
+    % Collecting stats 
+    
+        S  = zeros(NumBits*7*length(EbNo),1);
+        NS = S;
+    
+
+
+    for i = 1:length(EbNo)
+
+        % Signal to Noise Ratio dB
+        
+        SNR = EbNo(i) + 10*log10(codeRate)+10*log10(log2(M)); 
+
+        errorStats = zeros(3,1); %Reset the errorStats variable
+
+        count = 0;
+
+        while errorStats(3) < NumBits
+            
+            % For plotting the constellations, don't mind
+            
+                count = count + 1; 
+            
+            % Lets generate the message
+            
+                msgTx = randi([0 1],k*log2(M),1);
+            
+            % Lets encode the message
+            
+                encTx = encoder(msgTx);
+             
+            % Lets now modulate the encoded message 
+
+                modTx = qammod(encTx,M,'UnitAveragePower',true,...
+                               'InputType','bit');
+                           
+                % For plotting the constellations, again don't mind it           
+                    
+                    S(7*count:6 + 7*count,1) = modTx;
+                    
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            %%%%%%%%%%%%%%%%%%%%%THE CHANNEL%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                
+                % Add AWG Noise
+                    
+                    noisyRx = awgn(modTx,SNR); 
+                
+                    % For plotting the constellations, again don't mind it 
+
+                        NS(7*count:6 + 7*count,1) = noisyRx;
+
+
+            %%%%%%%%%%%%%%%%%%%%%%%%% END OF THE CHANNEL %%%%%%%%%%%%%%%%%%
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+         
+            
+            %                      THE OTHER SIDE...
+     
+         
+
+            % Lets Demodulate
+
+                demodRx = qamdemod(noisyRx,M,'UnitAveragePower',true,...
+                                   'OutputType','bit');
+            
+            % Now lets Decode the demodulated signal
+
+                msgRx = decoder(demodRx);
+                
+                
+            % Compute the errors due to the channel
+          
+                errorStats = errorRate(msgTx,msgRx);
+            
+        end
+        
+        % Get the BER for this point of Eb/No
+            
+            BER(i) = errorStats(1);
+        
+        % House Keeping
+            
+            reset(errorRate)
+
+    end
+
+
+
+end
--- Code/main.m
+++ Code/main.m
@@ -2,114 +2,28 @@ clc
 clear
 close all
 
-% Foward Error Correction
-% BCH Encoder and Decoder
 
-m = 3;
-K = 4;                    % Message length
-
-[encoder,decoder,codeRate] = bchFEC(m,K);
+EbNo = 0:30;            % Signal to Noise Ratio
+NumBits = 12000;        % Number of bits to proces
 
 
-% Modulation
-% 16 QAM Modulator
-
-M = 16;                     % Size of signal constellation
-b = log2(M);                % Number of bits per symbol
-
-% The noise?
-
-EbNo = 0:30;
-
-% Error Collecor
-
-errorRate = comm.ErrorRate;
-
-% Lets create the message
-
-% msgTx = randi([0 1],K*log2(M),1); % Make the message a factor the FEC and Modulation order
-% print(msgTx)  
-
-% Lets Encode
-
-%encTx = encoder(msgTx);
-%print(encTx')
-% Lets Modulate 
-
-% modTx = qammod(encTx,M,'UnitAveragePower',true,...
-%                             'InputType','bit');
-
-
-%s = scatterplot(modTx,1,0,'r*');
-%%hold on
-[~, q] = size(EbNo);
-BER = zeros(1,q);
-
-noBits = 12000;
-
-a = zeros(noBits*7*q,1);
-b = a;
-
-yes = true;
-
-for i = 1:q
+% BCH(7,4) -- 16 QAM
     
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%  THE CHANNEL  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    SNR = EbNo(i) + 10*log10(codeRate)+10*log10(log2(M)); % Signal to Noise Ratio dB
-    
-    errorStats = zeros(3,1); % Error Collector
-    
-    count = 0;
-    
-    while errorStats(3) < noBits
-        count = count + 1;
-        msgTx = randi([0 1],K*log2(M),1);
-        
-        encTx = encoder(msgTx);
-        
-        modTx = qammod(encTx,M,'UnitAveragePower',true,...
-            'InputType','bit');
-        a(7*count:6 + 7*count,1) = modTx;
-        
-        noisyRx = awgn(modTx,SNR); % Add AWG Noise
-        
-         b(7*count:6 + 7*count,1) = noisyRx;
-        
-        
-        %%%%%%%%%%%%%%%%%%%%%%%%% END OF THE CHANNEL %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        % %         s = scatterplot(modTx,1,0,'b');
-        % %         THE OTHER SIDE...
-        % %         hold on
-        % %         scatterplot(noisyRx,1,0,'r',s)
-        % %         grid on
-        
-        % Lets Demodulate
-        
-        demodRx = qamdemod(noisyRx,M,'UnitAveragePower',true,...
-            'OutputType','bit');
-        %print(demodRx')
-        
-        % Lets Decode
-        
-        msgRx = decoder(demodRx);
-        %print(msgRx')
-        errorStats = errorRate(msgTx,msgRx);
-        %errorStats = errorRate(msgTx,msgRx)
-    end
+    m = 3;                  % Galois field power
+    K = 4;                  % Message length
+    M = 16;                 % 16 QAM Modulator
    
-    BER(i) = errorStats(1);
-    reset(errorRate)
-    
-end
+   [BER,ModS,NoisyS] = computeBER(m,K,M,EbNo,NumBits);
 
-s = scatterplot(b,1,0,'r.');
-        % THE OTHER SIDE...
-hold on
-scatterplot(a,1,0,'g*',s)
-grid on
-figure
-hold off
-semilogy(EbNo, BER,'go-')
+   % Plot the constellation
+   
+   s = scatterplot(NoisyS,1,0,'r.');
+   hold on
+   scatterplot(ModS,1,0,'g*',s)
+   grid on
+   
+   % Plot the BER vs EbNo graph
+   
+   figure
+   hold off
+   semilogy(EbNo, BER,'g')
