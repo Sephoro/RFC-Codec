33862610e3434824add224b2a688848dac321768
sephoro <eliassepuru@gmail.com>
2019-10-06 12:22:38 +0200

    Able to plot multiple BER vs Eb/No graphs

--- Code/main.m
+++ Code/main.m
@@ -14,4 +14,23 @@ NumBits = 12000;        % Number of bits to proces
     M = 16;                 % 16 QAM Modulator
    
     [BER,ModS,NoisyS] = computeBER(m,K,M,EbNo,NumBits);
-    plots(EbNo,BER,ModS,NoisyS,m,K,M);
+    %plots(EbNo,BER,ModS,NoisyS,m,K,M);
+    
+% BCH(7,4) -- 64 QAM
+    
+    m1 = 3;                  % Galois field power
+    K1 = 4;                  % Message length
+    M1 = 64;                 % 64 QAM Modulator
+   
+   [BER1,ModS1,NoisyS1] = computeBER(m1,K1,M1,EbNo,NumBits);
+   
+  BER2 = [BER;BER1];
+  ModS2 = [ModS;ModS1];
+  NoisyS2 = [NoisyS;NoisyS1];
+  m2 = [m m1];
+  K2 = [K K1];
+  M2 = [M M1];
+  
+  plots(EbNo,BER2,ModS2,NoisyS2,m2,K2,M2);
+  
+    
--- Code/plots.m
+++ Code/plots.m
@@ -4,28 +4,46 @@ function [] = plots(EbNo, BER, ModS, NoisyS,m,k,M)
    
     
      % Plot the BER vs EbNo graph
+     
+     C = ['g','r','y','c','k', 'b']; % color schemes
+     legend__ = [];                  % For the legends
    
+     [size_,~] = size(BER);
      
-        semilogy(EbNo, BER,'g')
-        title('BCH with M-QAM')
-        xlabel('Eb/No')
-        ylabel('BER')
+     for i = 1:size_
+        
+         semilogy(EbNo, BER(i,:),C(i))
+         title('BCH with M-QAM')
+         xlabel('Eb/No')
+         ylabel('BER')
+         legend_ = strcat("BCH(", num2str(2^m(i) - 1 ),",", num2str(k(i)),...
+                           ") with ", num2str(M(i)), "-QAM");
+         legend__ = [legend__ legend_]; % Pad the legend
+         legend(legend__);
+         
+         hold on
      
+     end
+       
+     
+     hold off
      % Plot the constellations
      
         figure
-        s = scatterplot(NoisyS,1,0,'k.');  % Noisy Signal
-        title_ = strcat('Constellation of BCH(', num2str(2^m-1) ...
-                         ,',', num2str(k), ') with ', num2str(M), '-QAM');
+        s = scatterplot(NoisyS(1,:),1,0,'k.');  % Noisy Signal
+        title_ = strcat('Constellation of BCH(', num2str(2^m(1)-1) ...
+                         ,',', num2str(k), ') with ', num2str(M(1)), '-QAM');
                      
         title(title_);
         hold on
         
-        scatterplot(ModS,1,0,'g*',s)       % Original Signal
+        scatterplot(ModS(1,:),1,0,'g*',s)       % Original Signal
         grid on
         legend('Noisy Constellation', 'Original Constellation')
         
         hold off
+       
+        
    
    
 end
